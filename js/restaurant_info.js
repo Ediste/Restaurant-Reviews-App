1261dd51eba16e1d77cc894e516023463b208806
Ediste <stephanedin@gmx.de>
2019-04-28 22:41:19 +0200

    added mapbox api key

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1IjoiZWRpc3RlOTMiLCJhIjoiY2p2MTl3dWR3MXM5bDRkbGpmNHF0M2cwdyJ9.T82RgJKQkrWIWk4VuxTh3A',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
