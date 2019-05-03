66773b192ef7fdf9205f6067ce4921a0e71fa7bc
Ediste <stephanedin@gmx.de>
2019-05-03 18:25:51 +0200

    fix: changed urls to work on git hub pages

--- sw.js
+++ sw.js
@@ -6,26 +6,26 @@ let staticCacheName = 'restaurant-app-v1';
 self.addEventListener('install', function(event) {
     event.waitUntil(
         caches.open(staticCacheName).then(function(cache) {
-            return cache.addAll(['/',
-                                '/index.html',
-                                '/restaurant.html',
-                                '/css/styles.css',
-                                '/css/styles_responsive.css',
-                                '/js/dbhelper.js',
-                                '/js/main.js',
-                                '/js/restaurant_info.js',
-                                '/data/restaurants.json',
-                                '/js/sw_register.js',
-                                '/img/1.jpg',
-                                '/img/2.jpg',
-                                '/img/3.jpg',
-                                '/img/4.jpg',
-                                '/img/5.jpg',
-                                '/img/6.jpg',
-                                '/img/7.jpg',
-                                '/img/8.jpg',
-                                '/img/9.jpg',
-                                '/img/10.jpg']);
+            return cache.addAll(['./',
+                                './index.html',
+                                './restaurant.html',
+                                './css/styles.css',
+                                './css/styles_responsive.css',
+                                './js/dbhelper.js',
+                                './js/main.js',
+                                './js/restaurant_info.js',
+                                './data/restaurants.json',
+                                './js/sw_register.js',
+                                './img/1.jpg',
+                                './img/2.jpg',
+                                './img/3.jpg',
+                                './img/4.jpg',
+                                './img/5.jpg',
+                                './img/6.jpg',
+                                './img/7.jpg',
+                                './img/8.jpg',
+                                './img/9.jpg',
+                                './img/10.jpg']);
         })
     );
 });
